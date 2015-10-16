3778aa6bb5ab753ceb86598bc7f3fc53ff99700c
dibadalu <1004205150@qq.com>
2015-10-16 11:15:41 +0800

    初始化项目

---
+++ ZJWeibo/ZJWeibo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZJWeibo
+//
+//  Created by 陈泽嘉 on 15/10/16.
+//  Copyright (c) 2015年 dibadalu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZJWeibo/ZJWeibo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZJWeibo
+//
+//  Created by 陈泽嘉 on 15/10/16.
+//  Copyright (c) 2015年 dibadalu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

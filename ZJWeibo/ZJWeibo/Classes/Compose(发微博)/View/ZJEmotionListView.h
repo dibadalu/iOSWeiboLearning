61990adb2253617e7f2e433b7c02e47a417857dc
dibadalu <1004205150@qq.com>
2015-10-26 09:57:59 +0800

    重新添加ZJEmotionListView类

---
+++ ZJWeibo/ZJWeibo/Classes/Compose(发微博)/View/ZJEmotionListView.h
@@ -0,0 +1,14 @@
+//
+//  ZJEmotionlistView.h
+//  ZJWeibo
+//
+//  Created by dibadalu on 15/10/25.
+//  Copyright (c) 2015年 dibadalu. All rights reserved.
+//  表情键盘上的表情内容
+
+#import <UIKit/UIKit.h>
+
+@interface ZJEmotionListView : UIView
+
+
+@end
---
+++ ZJWeibo/ZJWeibo/Classes/Compose(发微博)/View/ZJEmotionListView.m
@@ -0,0 +1,28 @@
+//
+//  ZJEmotionlistView.m
+//  ZJWeibo
+//
+//  Created by dibadalu on 15/10/25.
+//  Copyright (c) 2015年 dibadalu. All rights reserved.
+//
+
+#import "ZJEmotionListView.h"
+
+@implementation ZJEmotionListView
+
+#pragma mark - 系统方法
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        self.backgroundColor = [UIColor redColor];
+        
+    }
+    
+    return self;
+}
+
+
+
+
+@end
